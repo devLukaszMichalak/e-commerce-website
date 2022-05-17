5533f48a4f42215e8f28446341060db8eed87385
Łukasz Michalak <lukaszmichalak0402@gmail.com>
2022-05-17 16:26:28 +0200

    initial frontend commit

---
+++ frontend/angular-ecommerce/src/app/common/product.ts
@@ -0,0 +1,13 @@
+export class Product {
+
+    public sku: string;
+    name: string;
+    description: string;
+    unitPrice: number;
+    imageUrl: string;
+    unitsInStock: number;
+    dateCreated: Date;
+    lastUpdated: Date;
+
+}
+
