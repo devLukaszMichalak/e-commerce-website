f3b3d5b65b597ab20d7358c90659dd2b1d46b913
Łukasz Michalak <lukaszmichalak0402@gmail.com>
2022-05-18 12:08:09 +0200

    added search by category

--- frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Product } from 'src/app/common/product';
 import { ProductService } from 'src/app/services/product.service';
 
@@ -10,16 +11,32 @@ import { ProductService } from 'src/app/services/product.service';
 export class ProductListComponent implements OnInit {
 
   products: Product[];
+  currentCategoryId: number;
 
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService,
+              private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.listProducts();
+    this.activatedRoute.paramMap.subscribe(
+      () => {this.listProducts()}
+    );
+
   }
 
   listProducts(){
 
-  this.productService.getProductList().subscribe(
+  const hasCategoryId: boolean = this.activatedRoute.snapshot.paramMap.has('id');
+
+
+  if(hasCategoryId){
+    this.currentCategoryId = parseInt(this.activatedRoute.snapshot.paramMap.get('id'));
+
+  }
+  else{
+    this.currentCategoryId = 1;
+  }
+
+  this.productService.getProductList(this.currentCategoryId).subscribe(
     data => {this.products = data}
   )
   }
