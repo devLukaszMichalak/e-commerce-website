615a7467353b0db1eb33dee4743b29cc42709856
Łukasz Michalak <lukaszmichalak0402@gmail.com>
2022-05-20 12:39:39 +0200

    detail shoppling cart list view

--- frontend/angular-ecommerce/src/app/services/cart.service.ts
+++ frontend/angular-ecommerce/src/app/services/cart.service.ts
@@ -54,4 +54,26 @@ export class CartService {
     console.log(`totalPrice${totalPriceValue.toFixed(2)} totalQuantity=${totalQuantityValue}`)
 
   }
+
+  decrementQuantity(theCartItem: CartItem) {
+    theCartItem.quantity--;
+
+    if(theCartItem.quantity == 0){
+      this.remove(theCartItem);
+    }
+    else{
+      this.computeCartTotal();
+    }
+
+  }
+  remove(theCartItem: CartItem) {
+    const itemIndex:number = this.cartItems.findIndex(tempCartItem => tempCartItem.id === theCartItem.id);
+
+    if(itemIndex > -1){
+      this.cartItems.splice(itemIndex,1);
+
+      this.computeCartTotal();
+
+    }
+  }
 }
